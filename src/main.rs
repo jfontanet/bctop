3f21347e64f655b2ff5c449a618dbd55a6d5efb8
Jan Fontanet <jfontanet@vintra.io>
2022-12-13 11:19:21 +0100

    check for updates

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ use log::LevelFilter;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
+use reqwest;
+use serde::Deserialize;
+use std::error::Error;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let logfile = FileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
         .build("log/output.log")?;
@@ -35,5 +40,77 @@ async fn main() -> Result<()> {
     });
 
     start_ui(&app_ui).await?;
+    // Check for updates and print to stdout.
+    let cli = reqwest::Client::new();
+    let resp = cli
+        .get("https://api.github.com/repos/jfontanet/bctop/releases")
+        .header(reqwest::header::USER_AGENT, "jfontanet")
+        .send()
+        .await?
+        .json::<Vec<Release>>()
+        .await?;
+    println!("{:?}", resp);
+    let last_release = resp.iter().reduce(|a, b| {
+        let mut a_versions = a.name.split(".");
+        let a_major = a_versions
+            .next()
+            .unwrap()
+            .split("v")
+            .nth(1)
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+        let a_minor = a_versions.next().unwrap().parse::<i32>().unwrap();
+        let a_patch = a_versions.next().unwrap().parse::<i32>().unwrap();
+        let mut b_versions = b.name.split(".");
+        let b_major = b_versions
+            .next()
+            .unwrap()
+            .split("v")
+            .nth(1)
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+        let b_minor = b_versions.next().unwrap().parse::<i32>().unwrap();
+        let b_patch = b_versions.next().unwrap().parse::<i32>().unwrap();
+
+        if a_major > b_major {
+            a
+        } else if a_major < b_major {
+            b
+        } else if a_minor > b_minor {
+            a
+        } else if a_minor < b_minor {
+            b
+        } else if a_patch > b_patch {
+            a
+        } else if a_patch < b_patch {
+            b
+        } else {
+            a
+        }
+    });
+    if let Some(last_release) = last_release {
+        // asume that the new latest is major than the one installed.
+        if last_release.name != format!("v{}", VERSION) {
+            println!(
+                "New version available: {} (from: {}) \n Download here: {}",
+                last_release.name, VERSION, last_release.assets[0].browser_download_url
+            );
+        }
+    }
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct Release {
+    name: String, // version
+    assets: Vec<Asset>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Asset {
+    name: String,
+    browser_download_url: String,
+    state: String,
 }
